c2ad7d65772860ab26b95efda2bc52607042fc24
Pascal Mickelson <mickelsp@gmail.com>
2013-02-11 22:36:21 -0700

    Machine-readable version of source data
    Converted plain ASCII file downloaded from NIST website to a
    machine-readable comma-delimited file.  I used regular expressions to
    substitute commas for white space of various lengths. Now ready to
    import all data.

--- R/PhysicalConstants.R
+++ R/PhysicalConstants.R
@@ -1,3 +1,11 @@
 #This package loads physical constants from the NIST database of Fundamental Physical Constants.
 #The data was most recently downloaded as an ASCII file from the NIST website on 2013/02/06.
 
+#figure out units for fine structure constant, inverse fine structure, muon g factor, muon magnetic moment anomaly, 
+#neutron g factor, neutron-proton mass difference, proton g factor
+
+#physical_constants <- data.frame(matrix(ncol = 4,nrow = 2));
+
+setwd("~/Documents/github/PhysicalConstants/R")
+physical_constants <- read.table("physical_constants_data.txt", header = TRUE, sep = ",")
+
