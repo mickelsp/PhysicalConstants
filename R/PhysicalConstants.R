37ee5f7b62ff4ed682c943607db35c0cec600f16
Pascal Mickelson <mickelsp@gmail.com>
2013-04-26 16:13:17 -0700

    Constant names and value assignments
    Progress on reading in constant names and then assigning values to
    those constant names.

--- R/PhysicalConstants.R
+++ R/PhysicalConstants.R
@@ -7,5 +7,21 @@
 #physical_constants <- data.frame(matrix(ncol = 4,nrow = 2));
 
 setwd("~/Documents/github/PhysicalConstants/R")
-physical_constants <- read.table("physical_constants_data.txt", header = TRUE, sep = ",")
+physical_constants <- read.csv("physical_constants_data.txt", header = TRUE, sep = ",")
+#I had to pre-format some things in the text file of data
+#1) comma delimited; 2) take out spaces between segments of numbers and between numbers and the exponential 'e' (still need to finish this)
 
+numrows = 3;
+
+#Assign values to all constant names
+for(row in 1:numrows){
+  #Make name of constant into an R-readable variable name
+  constantName <- gsub(pattern = "\\ +", replacement = "_", x = toString(physical_constants[row,1]))
+  constantName <- gsub(pattern = "\\-+", replacement = "_", x = constantName)
+    #Still to do: handle special cases like {220}_lattice_spacing_of_silicon
+  #Assign a value to the newly formed variable name
+  assign(constantName[1], 2e-30)
+}
+
+#figure out how to turn a value in the table into an R-recognized numeric value
+as.numeric(levels(physical_constants[row,2]))[physical_constants[row,2]]
